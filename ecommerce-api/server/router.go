bfadf8ce30293b7f6f2c1d28f7605e7f2a6a2d92
donquixxy <agusary726@gmail.com>
2025-03-31 23:09:14 +0800

    add health-check

--- ecommerce-api/server/router.go
+++ ecommerce-api/server/router.go
@@ -4,8 +4,10 @@ import (
 	"ecommerce-api/commons"
 	"ecommerce-api/config"
 	middleware2 "ecommerce-api/server/middleware"
+	"fmt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"time"
 )
 
 type ModelHandler struct {
@@ -21,7 +23,7 @@ type Router struct {
 
 func NewRouter(opt commons.Model) *Router {
 	e := echo.New()
-
+	now := time.Now()
 	e.Validator = config.NewValidator()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
@@ -32,6 +34,16 @@ func NewRouter(opt commons.Model) *Router {
 
 	publicApi := e.Group("")
 
+	publicApi.GET("/health-check", func(c echo.Context) error {
+		uptime := time.Since(now)
+
+		uptimeString := formatUptime(uptime)
+		return c.JSON(200, map[string]interface{}{
+			"status": "ok",
+			"uptime": uptimeString,
+		})
+	})
+
 	privateApi := e.Group("api")
 	privateApi.Use(middleware2.ValidateJWT(opt))
 
@@ -39,5 +51,22 @@ func NewRouter(opt commons.Model) *Router {
 		PublicApi:  publicApi,
 		PrivateApi: privateApi,
 		Echo:       e,
+	}
+}
+
+func formatUptime(uptime time.Duration) string {
+	days := int(uptime.Hours() / 24)
+	hours := int(uptime.Hours()) % 24
+	minutes := int(uptime.Minutes()) % 60
+	seconds := int(uptime.Seconds()) % 60
+
+	if days > 0 {
+		return fmt.Sprintf("%d days, %d hours, %d minutes, %d seconds", days, hours, minutes, seconds)
+	} else if hours > 0 {
+		return fmt.Sprintf("%d hours, %d minutes, %d seconds", hours, minutes, seconds)
+	} else if minutes > 0 {
+		return fmt.Sprintf("%d minutes, %d seconds", minutes, seconds)
+	} else {
+		return fmt.Sprintf("%d seconds", seconds)
 	}
 }
